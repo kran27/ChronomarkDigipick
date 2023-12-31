f624449abeb9e24846209e12e26d04b4158cdbc2
Kran <56488228+kran27@users.noreply.github.com>
2023-12-30 16:40:55 -0800

    greatly improved rendering + optimization
    numerous changes with a much better final result. last update before game logic is implemented.

--- digipick.app.js
+++ digipick.app.js
@@ -1,93 +1,115 @@
-function drawCircleChunks(boolArray, row, ox, oy, scale) {
-    var centerX = g.getWidth() / 2;
-    var centerY = g.getHeight() / 2;
-    var outerRadius = Math.min(centerX, centerY) - 10 - 16 * row; // leave a small margin
-    var innerRadius = outerRadius - 10; // adjust this to change the thickness of the ring
-    var chunkAngle = Math.PI * 2 / boolArray.length;
+var lI = 0;
+const c = 119, cA = Math.PI / 16;
 
-    switch (row) {
-        case 0:
-            g.setColor(0.878, 0.961, 0.984);
-            break;
-        case 1:
-            g.setColor(0.878, 0.961, 0.984);
-            break;
-        default:
-            g.setColor(0.482, 0.6, 0.69);
-    }
+function dP(a, sA, eA, oR, iR, oX, oY, s) {
+    const cS = Math.cos(sA + a), sS = Math.sin(sA + a), cC = Math.cos((sA + eA) / 2), sC = Math.sin((sA + eA) / 2), cE = Math.cos(eA - a), sE = Math.sin(eA - a);
 
-    if (row == 0) {
-        outerRadius += 8;
-        g.drawCircle(centerX * scale + ox, centerY * scale + oy, (outerRadius - 5) * scale);
-        console.log((centerX * scale + ox) + (outerRadius - 5) * scale);
-    }
+    const p = [
+        // Outer radius, start angle
+        (c + oR * cS) * s + oX,
+        (c + oR * sS) * s + oY,
+        // Outer radius, center
+        (c + oR * cC) * s + oX,
+        (c + oR * sC) * s + oY,
+        // Outer radius, end angle
+        (c + oR * cE) * s + oX,
+        (c + oR * sE) * s + oY,
+        // Inner radius, end angle
+        (c + iR * cE) * s + oX,
+        (c + iR * sE) * s + oY,
+        // Inner radius, start angle
+        (c + iR * cS) * s + oX,
+        (c + iR * sS) * s + oY
+    ];
+    g.fillPolyAA(p, true);
+}
 
-    for (var i = 0; i < boolArray.length; i++) {
-        if (boolArray[i]) {
-            if (row == 0) {
-                var startAngle = i * chunkAngle + 0.05;
-                var endAngle = (i + 1) * chunkAngle - 0.05;
-                var step = chunkAngle / 3;
-            }
-            else {
-                var startAngle = i * chunkAngle;
-                var endAngle = (i + 1) * chunkAngle + 0.01; //adding this prevent totally borked rendering
-                var step = chunkAngle;
-            }
-            var points = [];
-            for (var angle = startAngle; angle <= endAngle; angle += step) {
-                points.push((centerX + outerRadius * Math.cos(angle)) * scale + ox, (centerY + outerRadius * Math.sin(angle)) * scale + oy);
-            }
-            for (var angle = endAngle; angle >= startAngle; angle -= step) {
-                points.push((centerX + innerRadius * Math.cos(angle)) * scale + ox, (centerY + innerRadius * Math.sin(angle)) * scale + oy);
-            }
-            g.fillPoly(points, true);
+function dK(i, oX, oY, s) {
+    let oR = c - 27;
+    const iR = oR * .88;
+    lI = iR;
+
+    const cX = c * s + oX, cY = c * s + oY;
+
+    oR += 2;
+    g.setColor(.66, .66, .66).drawCircleAA(cX, cY, (oR - 8) * s);
+    g.setColor(-1);
+    for (let j = 0; j < 32; j++) {
+        if (kA[i][j]) {
+            const sA = (j - .5) * cA;
+            dP(.08, sA, sA + cA, oR, iR, oX, oY, s);
         }
     }
 }
 
-g.clear();
-
-// array of 4 random arrays
-var keyArrays = new Array(4).fill().map(() => new Array(32).fill().map(() => Math.random() > 0.75));
-var blockArray = new Array(32).fill().map(() => Math.random() > 0.25);
-var blockArray2 = new Array(32).fill().map(() => Math.random() > 0.25);
-
-var inGame = true;
-var activeKey = 0;
-
-function draw() {
-    g.clear();
-    if (inGame) {
-        drawCircleChunks(keyArrays[activeKey], 0, 0, 0, 1);
-        drawCircleChunks(blockArray, 1, 0, 0, 1);
-        drawCircleChunks(blockArray2, 2, 0, 0, 1);
-    }
-    else {
-        drawCircleChunks(keyArrays[0], 0, 18, 18, 0.4);
-        drawCircleChunks(keyArrays[1], 0, 128, 18, 0.4);
-        drawCircleChunks(keyArrays[2], 0, 18, 128, 0.4);
-        drawCircleChunks(keyArrays[3], 0, 128, 128, 0.4);
+function eK() {
+    const oR = 96, iR = 80;
+    g.setColor(0);
+    for (let i = 0; i < 16; i++) {
+        const sA = i * (cA * 2);
+        dP(0, sA, sA + cA * 2, oR, iR, 0, 0, 1);
     }
 }
 
-draw();
+function dC(r) {
+    let oR = lI - 4;
+    const iR = oR * .88;
+    lI = iR;
 
-setWatch(function () {
+    const f = r == 0 ? 0 : r == 1 ? .66 : 1 / r;
+    g.setColor(f, f, f);
+
+    for (let i = 0; i < 32; i++) {
+        if (bA[r - 1][i]) {
+            const sA = (i - .5) * cA;
+            dP(.008 + .004 * r, sA, sA + cA, oR, iR, 0, 0, 1);
+        }
+    }
+}
+
+// array of 4 random arrays
+var kA = [,,,,].fill().map(() => new Array(32).fill().map(() => Math.random() > .8)), bA = [,,,,].fill().map(() => new Array(32).fill().map(() => Math.random() > .2));
+
+var iG = true, aK = 0;
+
+function d() {
+    g.clear();
+    if (iG) {
+        dK(aK, 0, 0, 1);
+        dC(1);
+        dC(2);
+        dC(3);
+        dC(4);
+    }
+    else {
+        dK(0, 18, 18, .4);
+        dK(1, 128, 18, .4);
+        dK(2, 18, 128, .4);
+        dK(3, 128, 128, .4);
+    }
+    lI = 0;
+}
+
+d();
+//Dickens.loadSurround();
+
+setWatch(_ => {
     // move elements in keyArray forward (last element becomes first)
-    keyArrays[activeKey].unshift(keyArrays[activeKey].pop());
-    draw();
+    eK();
+    kA[aK].unshift(kA[aK].pop());
+    dK(aK, 0, 0, 1);
 }, BTN1, { repeat: true, debounce: 50, edge: "rising" });
 
-setWatch(function () {
+setWatch(_ => {
     // move elements in keyArray backward (first element becomes last)
-    keyArrays[activeKey].push(keyArrays[activeKey].shift());
-    draw();
+    eK();
+    kA[aK].push(kA[aK].shift());
+    dK(aK, 0, 0, 1);
 }, BTN4, { repeat: true, debounce: 50, edge: "rising" });
 
-setWatch(function () {
-    inGame = !inGame;
+setWatch(_ => {
+    iG = !iG;
     draw();
 }, BTN3, { repeat: true, debounce: 50, edge: "rising" });
 
-setWatch(_ => load("clock.app.js"),BTN2,{edge:1});
+setWatch(_ => load("clock.app.js"), BTN2, { edge: "falling" });
