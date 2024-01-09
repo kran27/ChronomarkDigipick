f1865a48fff78e3726398a7fae585ec4a8074520
Kran <56488228+kran27@users.noreply.github.com>
2024-01-08 18:01:09 -0800

    optimizations, comments, etc.

--- digipick.app.js
+++ digipick.app.js
@@ -1,16 +1,24 @@
-var lI = 0;
-const c = 119, cA = Math.PI / 16;
+//some of this code is pretty ugly, but it's that way to save space
+//comments are used since they're removed in the minification process
+var lI = 0; // last inner radius
+const c = 119, cA = Math.PI / 16; // center, cy=hunk angle
 
-function dP(a, sA, eA, oR, iR, oX, oY, s) {
+function dP(a, sA, eA, oR, iR, oX, oY, s, iK) { // draw polygon: angle, start angle, end angle, outer radius, inner radius, offset x, offset y, scale, is key
     const cS = Math.cos(sA + a), sS = Math.sin(sA + a), cC = Math.cos((sA + eA) / 2), sC = Math.sin((sA + eA) / 2), cE = Math.cos(eA - a), sE = Math.sin(eA - a);
-
-    const p = [
+    // cosines and sines of the start, center, and end angles
+    var p = [
         // Outer radius, start angle
         (c + oR * cS) * s + oX,
-        (c + oR * sS) * s + oY,
+        (c + oR * sS) * s + oY
+    ];
+
+    if (!iK) {
         // Outer radius, center
-        (c + oR * cC) * s + oX,
-        (c + oR * sC) * s + oY,
+        p.push((c + oR * cC) * s + oX);
+        p.push((c + oR * sC) * s + oY);
+    }
+
+    p = p.concat([
         // Outer radius, end angle
         (c + oR * cE) * s + oX,
         (c + oR * sE) * s + oY,
@@ -20,66 +28,85 @@ function dP(a, sA, eA, oR, iR, oX, oY, s) {
         // Inner radius, start angle
         (c + iR * cS) * s + oX,
         (c + iR * sS) * s + oY
-    ];
+    ]);
     g.fillPolyAA(p, true);
 }
 
-function dK(i, oX, oY, s) {
-    let oR = c - 27;
-    const iR = oR * .88;
+function dK(i, oX, oY, s) { // draw key: index, offset x, offset y, scale
+    let oR = c - 27; // outer radius
+    const iR = oR * .88; // inner radius
     lI = iR;
 
-    const cX = c * s + oX, cY = c * s + oY;
+    const cX = c * s + oX, cY = c * s + oY; // center x, center y
 
     oR += 2;
     g.setColor(.66, .66, .66).drawCircleAA(cX, cY, (oR - 8) * s);
-    g.setColor(-1);
+    g.setColor(-1); // -1 is white
     for (let j = 0; j < 32; j++) {
         if (kA[i][j]) {
-            const sA = (j - .5) * cA;
-            dP(.08, sA, sA + cA, oR, iR, oX, oY, s);
+            const sA = (j - .5) * cA; // start angle
+            dP(.08, sA, sA + cA, oR, iR, oX, oY, s, true);
         }
     }
 }
 
-function eK() {
-    const oR = 96, iR = 80;
+function eK() { // erase key, draws over key with background colour
+    const oR = 97, iR = 81; // outer radius, inner radius, small as possible to cover key but not waste time drawing
     g.setColor(0);
     for (let i = 0; i < 16; i++) {
         const sA = i * (cA * 2);
-        dP(0, sA, sA + cA * 2, oR, iR, 0, 0, 1);
+        dP(0, sA, sA + cA * 2, oR, iR, 0, 0, 1, true);
     }
 }
 
-function dC(r) {
-    let oR = lI - 4;
-    const iR = oR * .88;
+function dC(r, h) {// draw chunks: radius, is highlighted
+    let oR = lI - 4; // outer radius
+    const iR = oR * .88; // inner radius
     lI = iR;
 
-    const f = r == 0 ? 0 : r == 1 ? .66 : 1 / r;
-    g.setColor(f, f, f);
+    const f = r == 0 ? 0 : r == 1 ? .66 : 1 / r; // fill colour
+    const a = h ? .33 : 0; // ideally i'd save space by not declaring a, but that fucks up colours somehow
+    g.setColor(f, f, f + a);
 
     for (let i = 0; i < 32; i++) {
         if (bA[r - 1][i]) {
             const sA = (i - .5) * cA;
-            dP(.008 + .004 * r, sA, sA + cA, oR, iR, 0, 0, 1);
+            dP(.008 + .004 * r, sA, sA + cA, oR, iR, 0, 0, 1, false);
         }
     }
 }
 
-// array of 4 random arrays
-var kA = [,,,,].fill().map(() => new Array(32).fill().map(() => Math.random() > .8)), bA = [,,,,].fill().map(() => new Array(32).fill().map(() => Math.random() > .2));
+function cF(k, b) { // can fit: key, block
+    for (let o = 0; o < 32; o++) { // go through all offsets
+        let f = true;
+        for (let i = 0; i < 32; i++) { // check all indices
+            const j = (i + o) % 32;
+            if (k[i] && b[j]) {
+                f = false;
+                break;
+            }
+        }
+        if (f) {
+            return true;
+        }
+    }
+    return false;
+}
 
+// blocks
+var kA = [, , , ,].fill().map(() => new Array(32).fill().map(() => Math.random() > .8)), bA = [, , , ,].fill().map(() => new Array(32).fill().map(() => Math.random() > .2));
+
+// in game, active key
 var iG = true, aK = 0;
 
-function d() {
+function d() { // redraw entire screen
     g.clear();
     if (iG) {
         dK(aK, 0, 0, 1);
-        dC(1);
-        dC(2);
-        dC(3);
-        dC(4);
+        dC(1, cF(kA[aK], bA[0]));
+        dC(2, cF(kA[aK], bA[1]));
+        dC(3, cF(kA[aK], bA[2]));
+        dC(4, cF(kA[aK], bA[3]));
     }
     else {
         dK(0, 18, 18, .4);
@@ -95,21 +122,48 @@ d();
 
 setWatch(_ => {
     // move elements in keyArray forward (last element becomes first)
-    eK();
-    kA[aK].unshift(kA[aK].pop());
-    dK(aK, 0, 0, 1);
-}, BTN1, { repeat: true, debounce: 50, edge: "rising" });
+    if (iG) {
+        eK();
+        kA[aK].unshift(kA[aK].pop());
+        dK(aK, 0, 0, 1);
+    }
+    else {
+        aK = 1;
+        iG = true;
+        d();
+    }
+}, BTN1, { repeat: true, edge: "rising" });
 
 setWatch(_ => {
     // move elements in keyArray backward (first element becomes last)
-    eK();
-    kA[aK].push(kA[aK].shift());
-    dK(aK, 0, 0, 1);
-}, BTN4, { repeat: true, debounce: 50, edge: "rising" });
+    if (iG) {
+        eK();
+        kA[aK].push(kA[aK].shift());
+        dK(aK, 0, 0, 1);
+    }
+    else {
+        aK = 0;
+        iG = true;
+        d();
+    }
+}, BTN4, { repeat: true, edge: "rising" });
 
 setWatch(_ => {
+    if (!iG) {
+        aK = 2;
+    }
     iG = !iG;
-    draw();
-}, BTN3, { repeat: true, debounce: 50, edge: "rising" });
+    d();
+}, BTN3, { repeat: true, edge: "rising" });
 
-setWatch(_ => load("clock.app.js"), BTN2, { edge: "falling" });
+setWatch(_ => {
+    if (iG) {
+        kA = [, , , ,].fill().map(() => new Array(32).fill().map(() => Math.random() > .8))
+        bA = [, , , ,].fill().map(() => new Array(32).fill().map(() => Math.random() > .2))
+    }
+    else {
+        aK = 3;
+        iG = true;
+    }
+    d();
+}, BTN2, { repeat: true, edge: "falling" });
